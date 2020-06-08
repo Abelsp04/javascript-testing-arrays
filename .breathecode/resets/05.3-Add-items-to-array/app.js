122042e84afaca9f689d6230c892aef99093f63d
Abel Segui <Abelsegui@hotmail.com>
2020-06-08 05:44:03 +0000

    5.3

--- .breathecode/resets/05.3-Add-items-to-array/app.js
+++ .breathecode/resets/05.3-Add-items-to-array/app.js
@@ -6,9 +6,6 @@ var arr = [4,5,734,43,45];
 // for(let i = 0; i<10;i++){
 //         your loop content here   
 // }
-for (i=0;i<10;i++)
-{arr.push(Math.floor(Math.random()*100))}
 //*****************
-
 
 console.log(arr);
