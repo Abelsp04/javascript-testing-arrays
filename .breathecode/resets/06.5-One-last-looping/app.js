5c7abc5985bfc8c56271eea7c43f7b68cde3aefc
Abel Segui <Abelsegui@hotmail.com>
2020-06-10 23:34:16 +0000

    7.1

--- .breathecode/resets/06.5-One-last-looping/app.js
+++ .breathecode/resets/06.5-One-last-looping/app.js
@@ -2,8 +2,6 @@ let mySampleArray = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','F
 
 //your code here
 let inverted = [];
-let variable=mySampleArray
 for(let i = variable.length-1; i>=0;i--){
-
     console.log(variable[i]);
 }
